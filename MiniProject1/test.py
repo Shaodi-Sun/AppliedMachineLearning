22ce76c8ed61a408cfd0bd936a9eef1cec563f84
Shaodi Sun <shaodi.sun@mail.mcgill.ca>
2019-09-18 17:54:31 -0400

    import scipy and matplotlib

--- MiniProject1/test.py
+++ MiniProject1/test.py
@@ -1,12 +1,16 @@
 import pip
 from pip._internal import main
 import os
-os.system('/bin/bash -c "pip install numpy"')
+os.system('/bin/bash -c "sudo pip install numpy matplotlib scipy"')
 import numpy as np
 import math
+import scipy
+import matplotlib
 
 main(['list'])
 main(['show', 'wheel'])
+print('scipy Version: '+scipy.__version__)
+print('matplotlib Version: '+matplotlib.__version__)
 print (np.cbrt(27))
 
 
