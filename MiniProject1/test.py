10c3cf446d46b3e276878c337cb0551661762a0a
Shaodi Sun <shaodi.sun@mail.mcgill.ca>
2019-09-18 17:17:43 -0400

    clean up

--- MiniProject1/test.py
+++ MiniProject1/test.py
@@ -2,8 +2,11 @@ import pip
 from pip._internal import main
 import os
 os.system('/bin/bash -c "pip install numpy"')
+import numpy as np
+import math
 
 main(['list'])
 main(['show', 'wheel'])
+print (np.cbrt(27))
 
 
