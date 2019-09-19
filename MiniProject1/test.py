5b5a17a864521c92b649c20ede7812b0db5419e1
Shaodi Sun <shaodi.sun@mail.mcgill.ca>
2019-09-18 20:23:07 -0400

    load data into numpy nd array

--- MiniProject1/test.py
+++ MiniProject1/test.py
@@ -1,16 +1,31 @@
 import pip
 from pip._internal import main
 import os
-os.system('/bin/bash -c "sudo pip install numpy matplotlib scipy"')
+os.system('/bin/bash -c "sudo pip install numpy matplotlib scipy pandas"')
 import numpy as np
 import math
 import scipy
 import matplotlib
+import pandas as pd
 
-main(['list'])
-main(['show', 'wheel'])
-print('scipy Version: '+scipy.__version__)
-print('matplotlib Version: '+matplotlib.__version__)
-print (np.cbrt(27))
+
+# main(['list'])
+# main(['show', 'wheel'])
+# print('scipy Version: '+scipy.__version__)
+# print('matplotlib Version: '+matplotlib.__version__)
+# print (np.cbrt(27))
+
+#download data file
+wineQualityRawData = os.system('/bin/bash -c "curl -O https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"')
+breastCancerRawData = os.system('/bin/bash -c "curl -O https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"')
+
+#convert datafile into numpy nd-array
+wineQualityDataFrame= pd.read_csv('winequality-red.csv', sep='\t|;|,|[|]', engine='python', header=None)
+wineQualityNumpyArray = wineQualityDataFrame.to_numpy()
+#print(wineQualityNumpyArray.shape) output (1600, 12)
+breastCancerNumpyArray= np.loadtxt('breast-cancer-wisconsin.data', dtype=str, delimiter=',')
+#print(breastCancerNumpyArray.shape) output(699, 11)
+
+#clean data and create binary classification
 
 
