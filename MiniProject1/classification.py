started fix fit function, still need to implement covariance matrix

MiniProject1/classification.py
@@ -78,19 +78,23 @@ class LDA:
         #self.w = np.zeros(N);
         self.p0 = N0/(N0+N1) 
         self.p1 = N1/(N0+N1)
+        self.mu0 = np.zeros(N0+N1)
+        self.mu1 = np.zeros(N0+N1)
+        self.covariance = np.zeros([N0+N1, N0+N1])
       
         
     
-    def fit(self,X, y, a):
+    def fit(self,X, C1, C2, N0, N1):
         """ Define a fit function, which takes the training data (i.e., X and y)
         —as well as other hyperparameters (e.g., the learning rate and/or number
         of gradient descent iterations)—as input. 
         This function should train your model by modifying the model parameters """
-        self.mu0 = np.zeros(X.shape[1])
-        self.mu1 = np.zeros(X.shape[1])
+        self.mu0 = np.mean(C1, axis = 0)
+        self.mu1 = np.mean(C2, axis = 0)
+        denominator = (N0 + N1)/2
         self.covariance = np.zeros([X.shape[1], X.shape[1]])
     
-    def predict(self,X):
+    def predict(self, X):
         """ Define a predict function, which takes a set of input points (i.e., X) 
         as input and outputs predictions (i.e., yˆ) for these points. Note that you
          need to convert probabilities to binary 0-1 predictions by thresholding 

MiniProject1/main.py
@@ -57,23 +57,25 @@ breastCancerArrayDropLastColumn= np.delete(breastCancerArrayRowsDeleted, np.s_[-
 # print(benignCount) 444
 # print(MaglignantCount) 239
 
-# rowsForBenign = np.where(breastCancerNumpyArray[:, 10] == "2")[0]
-# rowsForMaglignant= np.where(breastCancerNumpyArray[:, 10] == "4")[0]
+rowsForBenign = np.where(breastCancerArrayRowsDeleted[:, 10] == "2")[0]
+rowsForMaglignant= np.where(breastCancerArrayRowsDeleted[:, 10] == "4")[0]
+benignClass = np.array(breastCancerArrayDropLastColumn, dtype=np.float)[rowsForBenign, :]
+maglignantClass =  np.array(breastCancerArrayDropLastColumn, dtype=np.float)[rowsForMaglignant, :]
 
 #logistic regression 
-n = wineFeatures.shape[0]
-ls = logisticRegression(wineFeatures.shape[1])
-start = time.process_time()
-w = ls.fit(wineFeatures,qualityBinary,0.05,0.001)
-end = time.process_time()
-print("Training Time: %.f s" % (end-start))
-perdictedY = np.zeros(n)
-scsCount = 0
-for i in range(n):
-  perdictedY[i] = ls.predict(wineFeatures[i,:])
-  if (qualityBinary[i] == perdictedY[i]):
-    scsCount += 1
-print("Accuracy: %.2f %%" % (100*scsCount/n))
+# n = wineFeatures.shape[0]
+# ls = logisticRegression(wineFeatures.shape[1])
+# start = time.process_time()
+# w = ls.fit(wineFeatures,qualityBinary,0.05,0.001)
+# end = time.process_time()
+# print("Training Time: %.f s" % (end-start))
+# perdictedY = np.zeros(n)
+# scsCount = 0
+# for i in range(n):
+#   perdictedY[i] = ls.predict(wineFeatures[i,:])
+#   if (qualityBinary[i] == perdictedY[i]):
+#     scsCount += 1
+# print("Accuracy: %.2f %%" % (100*scsCount/n))
 # Number of Iterations to converge: 1916
 # Training Time: 32 s
 # Accuracy: 66.54 %
@@ -81,5 +83,9 @@ print("Accuracy: %.2f %%" % (100*scsCount/n))
 #LDA
 lda = LDA(benignCount, MaglignantCount)
 start = time.process_time()
-
+test = lda.fit(breastCancerArrayDropLastColumn, benignClass, maglignantClass, benignCount, MaglignantCount)
+end = time.process_time()
+predict = lda.predict(breastCancerArrayDropLastColumn); 
+print("Training Time: %.f s" % (end-start))
+print("predict is %.f" %predict)
 